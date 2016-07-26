8930d2da37f36977d17161dd078f601e147ab317
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-07-26 16:28:20 +0200

    Update auto_install_geopoppy.sh

--- install/auto_install_geopoppy.sh
+++ install/auto_install_geopoppy.sh
@@ -6,10 +6,10 @@
 ## tar xvfz /home/pi/geopoppy.tar --preserve --same-owner -C /
 #___________________________________________________________________
 # Commande d'installation
-### wget -P /home/pi 
+### wget -P /home/pi wget -P /home/pi https://raw.githubusercontent.com/jancelin/geo-poppy/master/install/auto_install_geopoppy.sh && chmod +x /home/pi/auto_install_geopoppy.sh && sh /home/pi/auto_install_geopoppy.sh
 
-#wget -P /home/pi https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_sig.tar &&
-#wget -P /home/pi https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_base.tar &&
+wget -P /home/pi https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_sig.tar &&
+wget -P /home/pi https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_base.tar &&
 tar xvfz /home/pi/geopoppy_sig.tar --preserve --same-owner -C /  &&
 tar xvfz /home/pi/geopoppy_base.tar --preserve --same-owner -C /  &&
 cd /home/GeoPoppy&&docker-compose up -d
