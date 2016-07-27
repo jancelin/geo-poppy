3080c203cc37586dd480ba171868b05803fb4b04
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-07-27 20:17:17 +0200

    Update auto_install_geopoppy.sh

--- install/auto_install_geopoppy.sh
+++ install/auto_install_geopoppy.sh
@@ -1,12 +1,7 @@
 #!/bin/sh 
-### Commande sauvegarde du dossier
-## tar zcvf /home/pi/geopoppy.tar /home/GeoPoppy
-
-### Commande de restauration du dossier
-## tar xvfz /home/pi/geopoppy.tar --preserve --same-owner -C /
-#___________________________________________________________________
+#
 # Commande d'installation
-### wget -P /home/pirate wget https://raw.githubusercontent.com/jancelin/geo-poppy/master/install/auto_install_geopoppy.sh; chmod +x /home/pirate/auto_install_geopoppy.sh; bash -x /home/pirate/auto_install_geopoppy.sh
+### wget -P /home/pirate wget https://raw.githubusercontent.com/jancelin/geo-poppy/master/install/auto_install_geopoppy.sh; chmod +x /home/pirate/auto_install_geopoppy.sh; sh /home/pirate/auto_install_geopoppy.sh
 
 wget -P /home/pirate https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/install_wifi_direct_rpi3.sh; chmod +x /home/pirate/install_wifi_direct_rpi3.sh; bash -x /home/pirate/install_wifi_direct_rpi3.sh &&
 mkdir /home/pi &&
@@ -30,3 +25,10 @@ echo " "
 echo "Julien ANCELIN "
 echo "https://github.com/jancelin/geo-poppy"
 
+###_____________________________________________________________________________
+## Commande de sauvegarde du dossier data
+## tar zcvf /home/pi/geopoppy.tar /home/GeoPoppy
+
+### Commande de restauration du dossier data
+## tar xvfz /home/pi/geopoppy.tar --preserve --same-owner -C /
+#___________________________________________________________________
