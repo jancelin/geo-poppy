3094c34114b3cceeee0502dd6e84fc225fa784a5
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-07-27 10:32:19 +0200

    Update auto_install_geopoppy.sh

--- install/auto_install_geopoppy.sh
+++ install/auto_install_geopoppy.sh
@@ -1,15 +1,16 @@
 #!/bin/sh 
 ### Commande sauvegarde du dossier
-## tar zcvf /home/pirate/geopoppy.tar /home/GeoPoppy
+## tar zcvf /home/pi/geopoppy.tar /home/GeoPoppy
 
 ### Commande de restauration du dossier
-## tar xvfz /home/pirate/geopoppy.tar --preserve --same-owner -C /
+## tar xvfz /home/pi/geopoppy.tar --preserve --same-owner -C /
 #___________________________________________________________________
 # Commande d'installation
-### wget -P /home/pirate wget -P /home/pirate https://raw.githubusercontent.com/jancelin/geo-poppy/master/install/auto_install_geopoppy.sh && chmod +x /home/pirate/auto_install_geopoppy.sh && sh /home/pirate/auto_install_geopoppy.sh
+### wget -P /home/pi wget -P /home/pi https://raw.githubusercontent.com/jancelin/geo-poppy/master/install/auto_install_geopoppy.sh && chmod +x /home/pi/auto_install_geopoppy.sh && sh /home/pi/auto_install_geopoppy.sh
 
-wget -P /home/pirate https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_sig.tar &&
-wget -P /home/pirate https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_base.tar &&
-tar xvfz /home/pirate/geopoppy_sig.tar --preserve --same-owner -C /  &&
-tar xvfz /home/pirate/geopoppy_base.tar --preserve --same-owner -C /  &&
+mkdir /home/pi &&
+wget -P /home/pi https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_sig.tar &&
+wget -P /home/pi https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_base.tar &&
+tar xvfz /home/pi/geopoppy_sig.tar --preserve --same-owner -C /  &&
+tar xvfz /home/pi/geopoppy_base.tar --preserve --same-owner -C /  &&
 cd /home/GeoPoppy&&docker-compose up -d
