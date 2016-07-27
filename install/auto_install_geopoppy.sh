705aa81f4bb00eb40e768af1fba008aba95c1cfd
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-07-27 16:05:16 +0200

    Update auto_install_geopoppy.sh

--- install/auto_install_geopoppy.sh
+++ install/auto_install_geopoppy.sh
@@ -8,10 +8,11 @@
 # Commande d'installation
 ### wget -P /home/pirate wget https://raw.githubusercontent.com/jancelin/geo-poppy/master/install/auto_install_geopoppy.sh; chmod +x /home/pirate/auto_install_geopoppy.sh; sh /home/pirate/auto_install_geopoppy.sh
 
-
+wget -P /home/pirate https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/install_wifi_direct_rpi3.sh; chmod +x /home/pirate/install_wifi_direct_rpi3.sh; bash -x /home/pirate/install_wifi_direct_rpi3.sh &&
 mkdir /home/pi &&
 wget -P /home/pi https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_sig.tar &&
 wget -P /home/pi https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_base.tar &&
 tar xvfz /home/pi/geopoppy_sig.tar --preserve --same-owner -C /  &&
 tar xvfz /home/pi/geopoppy_base.tar --preserve --same-owner -C /  &&
-cd /home/GeoPoppy&&docker-compose up -d
+mv /home/Geopoppy/docker-compose.yml /home/pirate/docker-compose.yml
+docker-compose up -d
