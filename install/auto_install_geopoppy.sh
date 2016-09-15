2443749ae979a959b87bfbdf3b2e08503371a388
ANCELIN Julien <julien.ancelin@inra.fr>
2016-09-15 11:49:03 +0200

    Update auto_install_geopoppy.sh

--- install/auto_install_geopoppy.sh
+++ install/auto_install_geopoppy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh 
 #
 # Commande d'installation
-### wget -P /home/pirate wget https://raw.githubusercontent.com/jancelin/geo-poppy/master/install/auto_install_geopoppy.sh; chmod +x /home/pirate/auto_install_geopoppy.sh; sh /home/pirate/auto_install_geopoppy.sh
+### wget -P --no-check-certificate /home/pirate wget https://raw.githubusercontent.com/jancelin/geo-P --no-check-certificateoppy/master/install/auto_install_geopoppy.sh; chmod +x /home/pirate/auto_install_geopoppy.sh; sh /home/pirate/auto_install_geopoppy.sh
 
-wget -P /home/pirate https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/install_wifi_direct_rpi3.sh; chmod +x /home/pirate/install_wifi_direct_rpi3.sh; bash -x /home/pirate/install_wifi_direct_rpi3.sh &&
+wget -P --no-check-certificate /home/pirate https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/install_wifi_direct_rpi3.sh; chmod +x /home/pirate/install_wifi_direct_rpi3.sh; bash -x /home/pirate/install_wifi_direct_rpi3.sh &&
 mkdir /home/pi &&
-wget -P /home/pi https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_sig.tar &&
-wget -P /home/pi https://github.com/jancelin/geo-poppy/raw/master/install/geopoppy_base.tar &&
-tar xvfz /home/pi/geopoppy_sig.tar --preserve --same-owner -C /  &&
-tar xvfz /home/pi/geopoppy_base.tar --preserve --same-owner -C /  &&
+wget -P --no-check-certificate /home/pi https://github.com/jancelin/geo-P --no-check-certificateoppy/raw/master/install/geopoppy_sig.tar &&
+wget -P --no-check-certificate /home/pi https://github.com/jancelin/geo-P --no-check-certificateoppy/raw/master/install/geopoppy_base.tar &&
+tar xvfz /home/pi/geopoppy_sig.tar --P --no-check-certificatereserve --same-owner -C /  &&
+tar xvfz /home/pi/geopoppy_base.tar --P --no-check-certificatereserve --same-owner -C /  &&
 cp /home/GeoPoppy/docker-compose.yml /home/pirate/ &&
 docker-compose up -d &&
 sleep 5
@@ -23,12 +23,12 @@ echo "* Connection Data Base avec PgAdminIII ou Qgis sur la même ip, port 5432,
 echo "* Construire ses projets Qgis dans le répertoire /home/GeoPoppy/lizmap/project pour les rendre accessibles"
 echo " "
 echo "Julien ANCELIN "
-echo "https://github.com/jancelin/geo-poppy"
+echo "https://github.com/jancelin/geo-P --no-check-certificateoppy"
 
 ###_____________________________________________________________________________
 ## Commande de sauvegarde du dossier data
 ## tar zcvf /home/pi/geopoppy.tar /home/GeoPoppy
 
 ### Commande de restauration du dossier data
-## tar xvfz /home/pi/geopoppy.tar --preserve --same-owner -C /
+## tar xvfz /home/pi/geopoppy.tar --P --no-check-certificatereserve --same-owner -C /
 #___________________________________________________________________
