2047ef427c7b3d818f8f2926812bfaa51b165a08
ANCELIN Julien <julien.ancelin@inra.fr>
2016-10-12 14:44:46 +0200

    Update sauv_data.sql

--- sync/sauv_data.sql
+++ sync/sauv_data.sql
@@ -1,3 +1,4 @@
+--audit table to store all modifications of database
 CREATE TABLE sauv_data
 (
   integrateur character varying,
@@ -8,7 +9,7 @@ CREATE TABLE sauv_data
   sauv json
 )
 ------------------------------------------------------
--- Function: sauv_data()
+-- Function: sauv_data() store to sauv_data table all db modification
 
 -- DROP FUNCTION sauv_data();
 
@@ -29,16 +30,31 @@ BEGIN
 END;
 $sauv$ language plpgsql;
 ----------------------------------------------------------
-SELECT
-    'CREATE TRIGGER sauv AFTER INSERT OR DELETE OR UPDATE ON ' 
-    || tbl_name.tab_name
-    || ' FOR EACH ROW EXECUTE PROCEDURE sauv_data();'AS trigger_creation_query
-FROM (
-    SELECT
-        quote_ident(table_schema) || '.' || quote_ident(table_name) as tab_name
-    FROM
-        information_schema.tables
-    WHERE
-        table_schema NOT IN ('pg_catalog', 'information_schema')
-        AND table_schema NOT LIKE 'pg_toast%'
-) AS tbl_name;
+--create trigger sauv (Function: sauv_data()) for all tables in the database, less views and table sauv_data
+DO
+LANGUAGE plpgsql
+$$
+DECLARE
+query text;
+BEGIN
+  FOR query IN
+	SELECT
+	    'CREATE TRIGGER sauv AFTER INSERT OR DELETE OR UPDATE ON ' 
+	    || tbl_name.tab_name
+	    || ' FOR EACH ROW EXECUTE PROCEDURE sauv_data();'AS trigger_creation_query
+	FROM (
+	    SELECT
+		quote_ident(table_schema) || '.' || quote_ident(table_name) as tab_name
+	    FROM
+		information_schema.tables
+	    WHERE
+		table_schema NOT IN ('pg_catalog', 'information_schema')
+		AND table_schema NOT LIKE 'pg_toast%'
+		AND table_name NOT IN (SELECT viewname FROM pg_views WHERE schemaname NOT IN('information_schema', 'pg_catalog'))
+		AND table_name IS NOT sauv_data
+	) AS tbl_name
+	LOOP
+		EXECUTE	query;
+	END LOOP;
+END;
+$$;
