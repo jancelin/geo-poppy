c7503faea1acaa01c346f4586c0f29957fe77f05
ANCELIN Julien <julien.ancelin@inra.fr>
2017-01-18 13:20:23 +0100

    Update replay.sql

--- sync/replay.sql
+++ sync/replay.sql
@@ -12,14 +12,18 @@ FOR query IN
 		'INSERT INTO ' || schema_bd || '.' || tbl
 		|| ' SELECT * FROM json_populate_recordset(null::' ||schema_bd || '.' || tbl || ',''' || sauv || ''')' --json
 		||' ON CONFLICT ('|| pk ||') DO UPDATE set '|| pk ||'=DEFAULT;' --new id pk
-		
+
 	WHEN action1 = 'UPDATE' THEN
 		--update
 		'INSERT INTO ' || schema_bd || '.' || tbl
 		|| ' SELECT * FROM json_populate_recordset(null::' ||schema_bd || '.' || tbl || ',''' || sauv || ''')'--json
-		||' ON CONFLICT ('|| pk ||') DO UPDATE set '|| pk || '='
-		|| ((json_array_elements(sauv)->>pk)::TEXT::NUMERIC ) ||';'--old id pk	
-		
+		||' ON CONFLICT ('|| pk ||') DO UPDATE set ('||
+		(select string_agg(s.j, ',') from (select json_object_keys((select json_array_elements(sauv) from sauv_data)) j ) s)
+		||')=(EXCLUDED.'||
+		(select string_agg(s.j, ',EXCLUDED.') from (select json_object_keys((select json_array_elements(sauv) from sauv_data)) j ) s)
+		||');'
+		--|| ((json_array_elements(sauv)->>pk)::TEXT::NUMERIC ) ||';'--old id pk	
+
 	WHEN action1 = 'DELETE' THEN
 		--delete
 		'DELETE FROM ' || schema_bd || '.' || tbl
