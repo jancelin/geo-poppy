cb1a06cc3f2ee10f0be9af5dca2017a859ad946a
ANCELIN Julien <julien.ancelin@inra.fr>
2017-01-19 11:02:04 +0100

    Update replay.sql

--- sync/replay.sql
+++ sync/replay.sql
@@ -5,34 +5,32 @@ DECLARE
 query text;
 BEGIN
 FOR query IN
-  SELECT
+  SELECT distinct 			-- for grouping
 	CASE
-	WHEN action1 = 'INSERT' THEN
-		--insert
-		'INSERT INTO ' || schema_bd || '.' || tbl
-		|| ' SELECT * FROM json_populate_recordset(null::' ||schema_bd || '.' || tbl || ',''' || sauv || ''')' --json
-		||' ON CONFLICT ('|| pk ||') DO UPDATE set '|| pk ||'=DEFAULT;' --new id pk
+	WHEN action1 = 'INSERT' THEN 	-- Writes the data insert procedure 
+		'INSERT INTO '||s.schema_bd||'.'||s.tbl
+		||' SELECT * FROM json_populate_recordset(null::'||s.schema_bd ||'.'||s.tbl||','''||s.sauv||''')'	--json
+		||' ON CONFLICT ('||s.pk||') DO UPDATE set '||s.pk||'=DEFAULT;' --new id pk
 
-	WHEN action1 = 'UPDATE' THEN
-		--update
-		'INSERT INTO ' || schema_bd || '.' || tbl
-		|| ' SELECT * FROM json_populate_recordset(null::' ||schema_bd || '.' || tbl || ',''' || sauv || ''')'--json
-		||' ON CONFLICT ('|| pk ||') DO UPDATE set'||
-		--'(' ||
-		' ''(''||' ||' string_agg(s.j, '','')'||'||'')'' from (select json_object_keys((select json_array_elements('''|| sauv ||'''))) j ) s'
-		||' = '||
-		' ''(EXCLUDED.''||' ||' string_agg(s.j, '',EXCLUDED.'')'||'||'')'' from (select json_object_keys((select json_array_elements('''|| sauv ||'''))) j ) s'
-		||';'
-		--|| ((json_array_elements(sauv)->>pk)::TEXT::NUMERIC ) ||';'--old id pk	
+	WHEN action1 = 'UPDATE' THEN --Writes the data update procedure
+		'INSERT INTO '||s.schema_bd||'.'||s.tbl
+		||' SELECT * FROM json_populate_recordset(null::'||schema_bd||'.'||tbl||','''||sauv||''')'--json
+		||' ON CONFLICT ('||s.pk||') DO UPDATE set ('||f.f||') = (EXCLUDED.'||f.g||');' -- list of fields	
 
-	WHEN action1 = 'DELETE' THEN
-		--delete
-		'DELETE FROM ' || schema_bd || '.' || tbl
-		||' WHERE ' || pk || '=' || 
-		((json_array_elements(sauv)->>pk)::TEXT::NUMERIC ) ||';'--old id pk	
+	WHEN action1 = 'DELETE' THEN --Writes the data delete procedure
+		'DELETE FROM ' || s.schema_bd || '.' || s.tbl
+		||' WHERE ' || s.pk || '=' || 
+		((json_array_elements(s.sauv)->>pk)::TEXT::NUMERIC ) ||';'--old id pk	
 	END
-  FROM sauv_data
-ORDER BY ts asc
+  FROM 	sauv_data s, --Call the sauv_data table
+	(select e.ts, string_agg(e.json, ',') f, string_agg(e.json, ',EXCLUDED.') g
+	from (select ts, json_object_keys(d.json) json
+	      from 
+	      (select ts, json_array_elements(sauv) json 
+	       from sauv_data) d 
+	     ) e
+	group by e.ts) f -- create a list of fields
+
 	LOOP
 	  EXECUTE query;
 	END LOOP;
