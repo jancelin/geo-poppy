0a87db32779a6d2754c420af2a0da4e1628f9f47
ANCELIN Julien <julien.ancelin@inra.fr>
2017-01-23 14:04:40 +0100

    Update search.sql

--- sync/search.sql
+++ sync/search.sql
@@ -1,1 +1,8 @@
-select * from sauv_data
+SELECT
+	ts,
+	action1,
+	pk,
+	(json_array_elements(s.sauv)->>pk)::TEXT::NUMERIC foo
+	FROM sauv_data s
+	WHERE replay = FALSE AND action1 = 'UPDATE' or action1 = 'DELETE'
+	ORDER BY foo
