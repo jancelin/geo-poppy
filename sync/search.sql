567499f719b26f5f071a2b852cdce2e59a7eb0db
ANCELIN Julien <julien.ancelin@inra.fr>
2017-01-23 15:51:31 +0100

    Update search.sql

--- sync/search.sql
+++ sync/search.sql
@@ -1,11 +1,11 @@
 --Gestion des conflits du update delete d'une même entité lors d'un replay
-	SELECT al.*,egal.count  --selectionne les données modifiées/supprimées par 1 à n utilisateurs
-	FROM    ( --sous select : toute les données + pk value
+	SELECT al.*,egal.doublon  --selectionne les données modifiées/supprimées par 1 à n utilisateurs
+	FROM    ( --sous-select : toute les données + pk value
 		SELECT *,(json_array_elements(s.sauv)->>pk)::TEXT::NUMERIC id
 		FROM sauv_data s 
 		) al,
-		( --sousselect : données modifiées plusieurs fois dans le update et delete
-		SELECT distinct pk, (json_array_elements(s.sauv)->>pk)::TEXT::NUMERIC id, count(pk)
+		( --sous-select : données modifiées plusieurs fois dans le update et delete
+		SELECT distinct pk, (json_array_elements(s.sauv)->>pk)::TEXT::NUMERIC id, cast (count(pk) as integer) doublon
 		FROM sauv_data s
 		WHERE replay = FALSE AND action1 = 'UPDATE' OR action1 = 'DELETE'
 		GROUP BY pk, id
