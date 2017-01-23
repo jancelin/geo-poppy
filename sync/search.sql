a93df9eee52dbcd4261de6ec5013db526adec840
ANCELIN Julien <julien.ancelin@inra.fr>
2017-01-23 14:43:19 +0100

    Update search.sql

--- sync/search.sql
+++ sync/search.sql
@@ -1,8 +1,22 @@
-SELECT
-	ts,
-	action1,
-	pk,
-	(json_array_elements(s.sauv)->>pk)::TEXT::NUMERIC foo
+SELECT al.*  --selectionne le ts des données modifié/supprimé par plusieurs utilisateurs
+
+FROM    (--sous select : tout les pk,id,ts
+	SELECT *,(json_array_elements(s.sauv)->>pk)::TEXT::NUMERIC id
+	FROM
+	sauv_data s 
+	) al,
+	(--sousselect : données modifiées plusieurs fois dans le update et delete
+	SELECT distinct pk, (json_array_elements(s.sauv)->>pk)::TEXT::NUMERIC id, count(pk)
 	FROM sauv_data s
-	WHERE replay = FALSE AND action1 = 'UPDATE' or action1 = 'DELETE'
-	ORDER BY foo
+	WHERE 
+	replay = FALSE AND action1 = 'UPDATE' OR action1 = 'DELETE'
+	GROUP BY pk, id
+	ORDER BY id
+	    ) egal
+WHERE
+al.pk = egal.pk 
+AND al.id = egal.id
+AND egal.count >1
+
+ORDER BY al.id ASC
+
