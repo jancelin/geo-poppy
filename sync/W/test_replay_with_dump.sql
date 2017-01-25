4b7e24e0b008f383482b36ad0bdb36062258554d
ANCELIN Julien <julien.ancelin@inra.fr>
2017-01-25 13:26:59 +0100

    Update test_replay_with_dump.sql

--- sync/W/test_replay_with_dump.sql
+++ sync/W/test_replay_with_dump.sql
@@ -1,3 +1,4 @@
+
 delete from sauv_data;
 
 --db_serveur connect to one or more GeoPoppy for dumping data on his sauv_data table
@@ -12,4 +13,18 @@ SELECT * from dblink('geo', 'select * from sauv_data;') as t(integrateur text, t
 
 SELECT dblink_disconnect('geo');
 
+SELECT
+dblink_connect('geo','host=0.0.0.0 port=5402
+ user=geomatik
+ password=geomatik
+ dbname=framboise_entomo');
+
+INSERT INTO sauv_data 
+SELECT * from dblink('geo', 'select * from sauv_data;') as t(integrateur text, ts timestamp with time zone, schema_bd text, tbl text, action1 text, sauv json, pk text);
+
+SELECT dblink_disconnect('geo');
+
+
+select no_replay();
+-- vérifier les conflits et les résoudres: view "conflict"
 select replay();
