c9b9a79b5373402c08936d4a820515d38c780740
ANCELIN Julien <julien.ancelin@inra.fr>
2017-01-25 11:18:29 +0100

    Update function_replay.sql

--- sync/function_replay.sql
+++ sync/function_replay.sql
@@ -12,9 +12,7 @@ CREATE TABLE public.sauv_data
   no_replay integer
 );
 ----------------------------------------
-
-
-
+--créer les vues serch et conflict avent de créer la fonction
 DROP FUNCTION replay();
 CREATE OR REPLACE FUNCTION replay() RETURNS table(f1 boolean) AS
 $BODY$
@@ -44,13 +42,13 @@ SELECT x.q FROM (												--Keep only the replay req
 		||' UPDATE sauv_data SET replay = TRUE WHERE ts = '''||s.ts||''';'				--check TRUE on sauv_data when replay
 	END q
 	, s.ts													--s.ts for order by timestamp
-  FROM 	sauv_data  s, 												--CALL the sauv_data table
+  FROM 	search  s, 												--CALL the sauv_data table
 	(select e.ts, string_agg(e.json, ',') f,								--list of fields for upsert update
 	 string_agg(e.json,',EXCLUDED.') g 									--list of fileds for upsert update + EXCLUDED.
 	 from (select ts, json_object_keys(d.json) json								--list fields on json
 	      from 
 	      (select ts, json_array_elements(sauv) json 							--read ts & json array
-	       from sauv_data) d 
+	       from search) d 
 	     ) e
 	 group by e.ts) f 											--CALL list of fields
   WHERE s.ts = f.ts AND s.replay = FALSE
