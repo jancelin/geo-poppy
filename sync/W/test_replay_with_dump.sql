4206d9c3eee83ce0d5be7c93dc3ec334649e29cc
ANCELIN Julien <julien.ancelin@inra.fr>
2017-04-05 11:02:56 +0200

    Update test_replay_with_dump.sql

--- sync/W/test_replay_with_dump.sql
+++ sync/W/test_replay_with_dump.sql
@@ -3,26 +3,26 @@ delete from sauv_data;
 
 --db_serveur connect to one or more GeoPoppy for dumping data on his sauv_data table
 SELECT
-dblink_connect('geo','host=0.0.0.0 port=5401
+dblink_connect('geo1','host=0.0.0.0 port=5432
  user=geomatik
  password=geomatik
  dbname=framboise_entomo');
 
 INSERT INTO sauv_data 
-SELECT * from dblink('geo', 'select * from sauv_data;') as t(integrateur text, ts timestamp with time zone, schema_bd text, tbl text, action1 text, sauv json, pk text);
+SELECT * from dblink('geo1', 'select  ''geo1'', ts, schema_bd, tbl, action1, sauv, pk from sauv_data;') as t( integrateur text, ts timestamp with time zone, schema_bd text, tbl text, action1 text, sauv json, pk text);
 
-SELECT dblink_disconnect('geo');
+SELECT dblink_disconnect('geo1');
 
 SELECT
-dblink_connect('geo','host=0.0.0.0 port=5402
+dblink_connect('geo2','host=0.0.0.0 port=5432
  user=geomatik
  password=geomatik
  dbname=framboise_entomo');
 
 INSERT INTO sauv_data 
-SELECT * from dblink('geo', 'select * from sauv_data;') as t(integrateur text, ts timestamp with time zone, schema_bd text, tbl text, action1 text, sauv json, pk text);
+SELECT * from dblink('geo2', 'select  ''geo2'', ts, schema_bd, tbl, action1, sauv, pk from sauv_data;') as t(integrateur text, ts timestamp with time zone, schema_bd text, tbl text, action1 text, sauv json, pk text);
 
-SELECT dblink_disconnect('geo');
+SELECT dblink_disconnect('geo2');
 
 
 select no_replay();
