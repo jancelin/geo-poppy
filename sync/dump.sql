c467fad9272c60b1784e6a5e2bee13de5e820c6e
ANCELIN Julien <julien.ancelin@inra.fr>
2017-04-05 16:25:44 +0200

    Update dump.sql

--- sync/dump.sql
+++ sync/dump.sql
@@ -1,11 +1,22 @@
 --db_serveur connect to one or more GeoPoppy for dumping data on his sauv_data table
 SELECT
-dblink_connect('geo','host=172.24.1.1 port=5432
- user=docker
- password=docker
+dblink_connect('geo1','host=0.0.0.0 port=5432
+ user=geomatik
+ password=geomatik
  dbname=framboise_entomo');
 
-INSERT INTO sauv_data 
-SELECT * from dblink('geo', 'select * from sauv_data;') as t(integrateur text, ts timestamp with time zone, schema_bd text, tbl text, action1 text, sauv json, pk text);
+INSERT INTO sync.sauv_data 
+SELECT * from dblink('geo1', 'select  ''geo1'', ts, schema_bd, tbl, action1, sauv, pk from sauv_data;') as t( integrateur text, ts timestamp with time zone, schema_bd text, tbl text, action1 text, sauv json, pk text);
 
-SELECT dblink_disconnect('geo');
+SELECT dblink_disconnect('geo1');
+
+SELECT
+dblink_connect('geo2','host=0.0.0.0 port=5432
+ user=geomatik
+ password=geomatik
+ dbname=framboise_entomo');
+
+INSERT INTO sync.sauv_data 
+SELECT * from dblink('geo2', 'select  ''geo2'', ts, schema_bd, tbl, action1, sauv, pk from sauv_data;') as t(integrateur text, ts timestamp with time zone, schema_bd text, tbl text, action1 text, sauv json, pk text);
+
+SELECT dblink_disconnect('geo2');
