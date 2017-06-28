0778785c2525c98d879ba474d7c9d49403d1ae96
ANCELIN Julien <julien.ancelin@inra.fr>
2017-06-28 16:56:35 +0200

    Update param.inc.php

--- collec/build/param.inc.php
+++ collec/build/param.inc.php
@@ -17,11 +17,11 @@ $OBJETBDD_debugmode = 1;
 /*
  * code de l'application dans la gestion des droits
  */
-$GACL_aco = "col";
+$GACL_aco = "zaalpes";
 /*
  * Code de l'application - impression sur les etiquettes
  */
-$APPLI_code = 'proto';
+$APPLI_code = 'zaalpes';
 /*
  * Mode d'identification
  * BDD : uniquement a partir des comptes internes
@@ -40,14 +40,14 @@ $ident_type = "BDD";
 $BDD_login = "collec";
 $BDD_passwd = "collec";
 $BDD_dsn = "pgsql:host=postgiscollec;dbname=collec;sslmode=require";
-$BDD_schema = "col,gacl,public";
+$BDD_schema = "zaalpes,gacl,public";
 
 /*
  * Rights management, logins and logs records database
  */
 $GACL_dblogin = "collec";
 $GACL_dbpasswd = "collec";
-$GACL_aco = "col";
+$GACL_aco = "zaalpes";
 $GACL_dsn = "pgsql:host=postgiscollec;dbname=collec;sslmode=require";
 $GACL_schema = "gacl";
 
@@ -111,14 +111,13 @@ $mapDefaultZoom = 7;
         *   */
 $APPLI_mail = "geopoppy@geopoppy.com";
 $APPLI_nom = "Prototype d'application";
-$APPLI_code = 'collec-master';
+//$APPLI_code = 'collec-develop';
 $APPLI_fds = "display/CSS/blue.css";
-$APPLI_address = "http://172.24.1.1/collec-master";
+$APPLI_address = "http://172.24.1.1/collec-develop";
 $APPLI_modeDeveloppement = false;
 $APPLI_modeDeveloppementDroit = false;
 $APPLI_utf8 = true;
 $APPLI_menufile = "param/menu.xml";
 $APPLI_temp = "temp";
-$APPLI_titre = "Gestion des échantillons";
+$APPLI_titre = "Gestion des échantillons ZA Alpes";
 ?>
-
