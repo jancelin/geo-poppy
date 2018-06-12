bc3f71cb9d8daca8edf363d88b779531631b0615
ANCELIN Julien <julien.ancelin@inra.fr>
2018-06-12 10:16:23 +0200

    Update check_docker.sh

--- install/check_docker.sh
+++ install/check_docker.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-### BEGIN INIT INFO
-# Provides:          scriptname
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start daemon at boot time
-# Description:       Enable service provided by daemon.
-### END INIT INFO
-
 # Author: Erik Kristensen
 # Email: erik@erikkristensen.com
 # License: MIT
@@ -28,45 +18,24 @@
 # sudo chmod +x /etc/init.d/check_docker
 # sudo update-rc.d check_docker defaults 80 
 
-
-sleep 60
+#set -x
+#sleep 60
 LIST=$(docker ps -aq)
 for CONTAINER in $LIST
 do
 
-  if [ "x${CONTAINER}" == "x" ]; then
-    echo "UNKNOWN - Container ID or Friendly Name Required"
-    #exit 3
-  fi
+  RUNNING=$(docker inspect --format="{{.State.Running}}" $CONTAINER )
 
-  if [ "x$(which docker)" == "x" ]; then
-    echo "UNKNOWN - Missing docker binary"
-    #exit 3
-  fi
-
-  docker info > /dev/null 2>&1
-  if [ $? -ne 0 ]; then
-    echo "UNKNOWN - Unable to talk to the docker daemon"
-    #exit 3
-  fi
-
-  RUNNING=$(docker inspect --format="{{.State.Running}}" $CONTAINER 2> /dev/null)
-
-  if [ $? -eq 1 ]; then
-    echo "UNKNOWN - $CONTAINER does not exist."
-    #exit 3
-  fi
-
-  if [ "$RUNNING" == "false" ]; then
+  if [ "$RUNNING" = "false" ]; then
     echo "CRITICAL - $CONTAINER is not running."
     docker-compose down --remove-orphans &&
     docker-compose up -d
-    #exit 2
+    exit
   fi
 
   RESTARTING=$(docker inspect --format="{{.State.Restarting}}" $CONTAINER)
 
-  if [ "$RESTARTING" == "true" ]; then
+  if [ "$RESTARTING" = "true" ]; then
     echo "WARNING - $CONTAINER state is restarting."
     #exit 1
   fi
