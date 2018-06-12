c7cc60cd3db2d50c3fc1859bddb3f627ce98c702
ANCELIN Julien <julien.ancelin@inra.fr>
2018-06-12 14:59:50 +0200

    Update check_docker.sh

--- install/check_docker.sh
+++ install/check_docker.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 ### BEGIN INIT INFO
-# Provides: check_docker.sh #Le nom de votre script
-# Required-Start:    $remote_fs $syslog #Je ne sais pas du tout ce que c'est
-# Required-Stop:     $remote_fs $syslog #Même problème
-# Default-Start:     2 3 4 5 #J'ai rien compris sur ces niveaux
-# Default-Stop:      0 1 6 #J'ai simplement compris que c'est les niveaux qui restent
-# Short-Description: refabriquer les services si il sont en exit àprès un reboot
-# Description: # Une description complète ici
+# Provides:          chech_docker
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Description courte
+# Description:       Description longue
 ### END INIT INFO
+
 # Author: Erik Kristensen
 # Email: erik@erikkristensen.com
 # License: MIT
@@ -18,13 +19,7 @@
 #
 # List all container in a docker-compose 
 # and If one or more is exit, it do a docker-compose down and up
-# 
 
-#BEFORE:
-# 
-# sudo nano /etc/init.d/check_docker
-# sudo chmod +x /etc/init.d/check_docker
-# sudo update-rc.d check_docker defaults 80 
 
 #set -x
 sleep 40
