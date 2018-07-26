702dae427cc569b8da9df038ddf5b363e9b62970
CorentinFalc <akwalith@gmail.com>
2018-07-26 09:48:17 +0200

    Edit print

--- tracking/GPS1.py
+++ tracking/GPS1.py
@@ -89,7 +89,7 @@ while True:
             if DEBUG is True: 
                 print "Writing into database..." 
             
-            print "Début de l'insertion"
+            print "Starting insert !"
             cus_date = datetime.strptime(gpsData['fix_date'], "%d%m%Y").date()                
             
             # Data to insert 
