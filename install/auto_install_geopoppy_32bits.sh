c20ec246f1d342bfda86c837354d47b9648d7f40
Pierre <pierrechevaliergeol@gmail.com>
2018-08-10 10:19:02 +0200

    Update auto_install_geopoppy_32bits.sh
    Added a " at the end of an echo. And a few other meaningless details.

--- install/auto_install_geopoppy_32bits.sh
+++ install/auto_install_geopoppy_32bits.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 #set -e
-# Commande d'installation
+# Commande d'installation:
 ### curl -fsSL https://raw.githubusercontent.com/jancelin/geo-poppy/master/install/auto_install_geopoppy.sh | sh
 
 wget --no-check-certificate -N -P /home/pirate https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/install_wifi_direct_rpi3.sh; chmod +x /home/pirate/install_wifi_direct_rpi3.sh; bash -x /home/pirate/install_wifi_direct_rpi3.sh &&
@@ -10,9 +10,9 @@ sleep 30
 
 	#Récupération du numéro de la ligne de l'entrypoint dans la variable fLine grâce à la commande awk
 fLine=$(awk '/entrypoint:/ { print NR}' docker-compose.yml)&&
-	#Création de la variable endLine pour récupérer la dernière ligne à commenter (On souhaite ici commenter 4 lignes au total)
+	#Création de la variable endLine pour récupérer la dernière ligne à commenter (on souhaite ici commenter 4 lignes au total)
 endLine=$((fLine+3))&&
-	#Parcours des 4 lignes à commenter dans une boucle for.
+	#Parcours des 4 lignes à commenter dans une boucle for
 for i in `seq $fLine  $endLine `
 do
 	#Utilisation de la commande sed dans la boucle pour remplacer l'espace du début de ligne par un #
@@ -30,11 +30,11 @@ echo "* Redémarrer le raspberry pour l'activation du wifi : sudo reboot"
 echo " "
 echo "* Connectez-vous ensuite au réseau wifi GeoPoppy_Pi3"
 echo " Mot de passe: geopoppy"
-echo " Puis tapper l'adresse https://172.24.1.1 dans votre navigateur web pour accéder à la démo"
+echo " Puis taper l'adresse https://172.24.1.1 dans votre navigateur web pour accéder à la démo"
 echo " IL FAUT PARAMETRER LIZMAP!!!! https://172.24.1.1/websig/lizmap/www/admin.php/admin/config/"
 echo " "
 echo "* Connection Data Base avec PgAdminIII ou Qgis 172.24.1.1:5432, base: geopoppy , login et mot de passe: docker"
-echo "* Déposer ses projets Qgis ici:  http://172.24.1.1:8000/fs/mnt/fs/files/qgis/ 
+echo "* Déposer ses projets Qgis ici:  http://172.24.1.1:8000/fs/mnt/fs/files/qgis/ "
 echo " "
 echo " en wifi direct: "
 echo " 172.24.1.1                >> lizmap / qgiserver "
