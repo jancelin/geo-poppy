d6c155c3365d7d812d2a0bb783c8489f4858555f
ANCELIN Julien <julien.ancelin@inra.fr>
2018-09-13 16:20:56 +0200

    Create server.py

---
+++ geolocate/server.py
@@ -0,0 +1,20 @@
+import SocketServer
+import json
+import os
+import subprocess
+import commands
+
+class MyTCPServerHandler(SocketServer.BaseRequestHandler):
+        
+        def handle(self):
+               
+            try:
+                data = json.loads(self.request.recv(1024).strip())
+                print data
+                self.request.sendall(json.dumps(data))
+            except Exception, e:
+                print "Exception wile receiving message: ", e
+
+server = SocketServer.TCPServer(("", 1979), MyTCPServerHandler)
+print '=== WE ARE LIVE ==='
+server.serve_forever()
